12fca868bc6cf03799a43855a47f55922e55e3eb
Jackson McDonald <94773420+JacksonVox@users.noreply.github.com>
2023-06-28 12:44:26 -0700

    daily codewars challenge

---
+++ Kyu8/YouOnlyNeedOneBeginner.js
@@ -0,0 +1,9 @@
+function check(a, x) {
+  return a.includes(x);
+}
+/*
+parameters:(a) array containing numbers or strings or subarrays, (x) value to check array for
+return: if (a) contains (x)
+example: ([1,2,3],3) => true
+pseudo-code: return a.includes(x)
+*/
